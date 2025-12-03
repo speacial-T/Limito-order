32daf5533186d554ac71432ced24a548ef747dd5
jjaenie <wogus990519@gmail.com>
2025-12-03 12:33:03 +0900

    feat (#2) : 레포지토리 수정, 한정판매 주문 생성 요청 dto구조 변경
    # Conflicts:
    #	src/main/java/com/limito/order/cart/domain/dto/limitedProduct/AddCartLimitedRequestV1.java
    #	src/main/java/com/limito/order/cart/domain/dto/limitedProduct/AddCartLimitedResponseV1.java

--- src/main/java/com/limito/order/order/domain/repository/OrderServiceRepositoryV1.java
+++ src/main/java/com/limito/order/order/domain/repository/OrderServiceRepositoryV1.java
@@ -1,6 +1,9 @@
 package com.limito.order.order.domain.repository;
 
+import com.limito.order.order.domain.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderServiceRepositoryV1 extends JpaRepository<> {
+import java.util.UUID;
+
+public interface OrderServiceRepositoryV1 extends JpaRepository<Order, UUID> {
 }
