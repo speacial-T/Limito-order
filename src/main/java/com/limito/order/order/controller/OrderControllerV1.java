c0d5750ac5ca01c075152b4e145b8b1c94862561
jjaenie <wogus990519@gmail.com>
2025-12-04 03:28:08 +0900

    feat (#2) : 변경된 클래스명 컨트롤러에 반영

--- src/main/java/com/limito/order/order/controller/OrderControllerV1.java
+++ src/main/java/com/limito/order/order/controller/OrderControllerV1.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.limito.order.order.domain.dto.request.CreateLimitedOrderResquestV1;
-import com.limito.order.order.domain.dto.response.LimitedOrderResponseV1;
+import com.limito.order.order.domain.dto.response.CreateLimitedOrderResponseV1;
 import com.limito.order.order.service.OrderServiceV1;
 
 import jakarta.validation.Valid;
@@ -20,11 +20,11 @@ public class OrderControllerV1 {
 	private final OrderServiceV1 orderService;
 
 	@PostMapping("/limited/direct")
-	public ResponseEntity<LimitedOrderResponseV1> createDirectLimitedOrder(
+	public ResponseEntity<CreateLimitedOrderResponseV1> createDirectLimitedOrder(
 		@Valid @RequestBody CreateLimitedOrderResquestV1 createLimitedOrderResquest) {
 		// Todo. userId 헤더에서 빼오기, 권한검증
 		Long userId = 1111L;
-		LimitedOrderResponseV1 result = orderService.createDirectLimitedOrder(userId, createLimitedOrderResquest);
+		CreateLimitedOrderResponseV1 result = orderService.createDirectLimitedOrder(userId, createLimitedOrderResquest);
 		return ResponseEntity.ok(result);
 	}
 }
