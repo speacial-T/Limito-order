347f4feb6cd19dc330e5ca348e51bc50084230b3
jjaenie <wogus990519@gmail.com>
2025-12-04 15:00:35 +0900

    feat (#2) : 한정판매 바로 구매 서비스 로직 구현

--- src/main/java/com/limito/order/order/service/OrderServiceV1.java
+++ src/main/java/com/limito/order/order/service/OrderServiceV1.java
@@ -6,35 +6,40 @@ import org.springframework.stereotype.Service;
 
 import com.limito.order.order.domain.dto.request.CreateLimitedOrderResquestV1;
 import com.limito.order.order.domain.dto.response.CreateLimitedOrderResponseV1;
+import com.limito.order.order.domain.mapper.OrderMapper;
 import com.limito.order.order.domain.model.Order;
 import com.limito.order.order.domain.model.OrderItem;
 import com.limito.order.order.domain.repository.OrderServiceRepositoryV1;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceV1 {
 	private final OrderServiceRepositoryV1 orderServiceRepository;
+	private final OrderMapper orderMapper;
 
 	public CreateLimitedOrderResponseV1 createDirectLimitedOrder(Long userId,
-		@Valid CreateLimitedOrderResquestV1 createLimitedOrderResquest) {
+		CreateLimitedOrderResquestV1 createLimitedOrderResquest) {
 
-		//requestDto -> entity 매핑해서 주문 엔티티 생성
-		Order order = Order.toEntity(createLimitedOrderResquest);
+		// requestDto -> entity 매핑해서 주문 엔티티 생성
+		Order order = orderMapper.toOrderEntity(userId, createLimitedOrderResquest);
+
+		// requestDto -> entity 매핑해서 주문 아이템 엔티티 생성
+		List<OrderItem> orderItems = orderMapper.toOrderItemEntity(createLimitedOrderResquest);
+		// 연관관계 설정
+		order.attachOrderItems(orderItems);
 
 		// itemSummary set 하는 함수 추가하기
-
-		//requestDto -> entity 매핑해서 주문 아이템 엔티티 생성
-		List<OrderItem> orderItems = OrderItem.toEntity(createLimitedOrderResquest);
-		//주문 아이템 주문 테이블에 추가...?
-		order.add(orderItems);
+		order.attachSummary(createLimitedOrderResquest);
 
 		// 생성된 주문 엔티티 저장
 		orderServiceRepository.save(order);
 
 		// 엔티티 -> responseDto 변환해서 리턴
+		//		List<CreateLimitedOrderItemResponseV1> orderItemRes = orderMapper.toLimitedOrderItemResponse(order);
+		CreateLimitedOrderResponseV1 limitedOrderRes = orderMapper.toLimitedOrderResponse(order);
 
+		return limitedOrderRes;
 	}
 }
