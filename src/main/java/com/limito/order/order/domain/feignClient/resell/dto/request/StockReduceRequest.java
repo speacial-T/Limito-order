96ad3ef2e6912d435f104b836f3d66923a5b401a
jjaenie <wogus990519@gmail.com>
2025-12-05 16:15:06 +0900

    feat (#28) : feign client 초기 설정

---
+++ src/main/java/com/limito/order/order/domain/feignClient/resell/dto/request/StockReduceRequest.java
@@ -0,0 +1,16 @@
+package com.limito.order.order.domain.feignClient.resell.dto.request;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+
+public class StockReduceRequest {
+	@NotNull
+	private UUID productId;
+
+	@NotNull
+	private UUID optionId;
+
+	@NotNull
+	private UUID stockId;
+}
