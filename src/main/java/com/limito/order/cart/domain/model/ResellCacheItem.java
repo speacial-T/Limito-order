6bb4a3c7b289dbd645543f69ace791c52cf8c9cf
jjaenie <wogus990519@gmail.com>
2025-12-06 23:43:15 +0900

    refactor (#29) : 레디스 조회 시 역직렬화 에러 해결 - 생성자 추가

--- src/main/java/com/limito/order/cart/domain/model/ResellCacheItem.java
+++ src/main/java/com/limito/order/cart/domain/model/ResellCacheItem.java
@@ -4,13 +4,17 @@ import java.util.UUID;
 
 import com.limito.order.common.ProductType;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResellCacheItem {
 	private UUID optionId;
 	private UUID stockId;
