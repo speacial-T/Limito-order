881f103b70649268f2784546767e44130dc41200
River is line <dmstjs7047@hanmail.net>
2025-12-10 03:50:18 +0900

    test (#42) : test를 위해 response class에 @Getter 추가

--- src/main/java/com/limito/order/order/domain/dto/response/GetOrdersForCompanyResponseV1.java
+++ src/main/java/com/limito/order/order/domain/dto/response/GetOrdersForCompanyResponseV1.java
@@ -4,8 +4,10 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class GetOrdersForCompanyResponseV1 {
 	private UUID orderId;
 	private String itemSummary;
