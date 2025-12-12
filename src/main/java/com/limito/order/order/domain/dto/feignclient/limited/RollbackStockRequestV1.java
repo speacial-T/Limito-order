78726e153f5d0252b058686dbe444190c882a008
jjaenie <wogus990519@gmail.com>
2025-12-12 11:40:08 +0900

    feat (#53) : 주문 취소 internal interface, requestDto 작성
    # Conflicts:
    #	src/main/java/com/limito/order/common/feignclient/LimitedFeignClient.java
    #	src/main/java/com/limito/order/order/domain/dto/feignclient/limited/OptionItemAmountRequest.java
    #	src/main/java/com/limito/order/order/domain/dto/feignclient/limited/RollbackStockRequestV1.java

--- src/main/java/com/limito/order/order/domain/dto/feignclient/limited/RollbackStockRequestV1.java
+++ src/main/java/com/limito/order/order/domain/dto/feignclient/limited/RollbackStockRequestV1.java
@@ -6,9 +6,11 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class RollbackStockRequestV1 {
 	@NotNull(message = "상품 목록은 null일 수 없습니다.")
