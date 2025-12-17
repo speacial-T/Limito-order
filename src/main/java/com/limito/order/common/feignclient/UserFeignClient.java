7b8901bd39334eb11796adca34b654dd3eb6cf51
jjaenie <wogus990519@gmail.com>
2025-12-17 10:24:40 +0900

    fix (#54) : HTTP 메서드 변경

--- src/main/java/com/limito/order/common/feignclient/UserFeignClient.java
+++ src/main/java/com/limito/order/common/feignclient/UserFeignClient.java
@@ -2,13 +2,13 @@ package com.limito.order.common.feignclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.limito.order.order.domain.dto.feignclient.user.OrderedUserInfoResponseV1;
 
 @FeignClient(name = "user-service", url = "${feign.user-service.url}")
 public interface UserFeignClient {
-	@PostMapping("/internal/api/v1/user/{userId}/ordered-user")
+	@GetMapping("/internal/api/v1/user/{userId}/ordered-user")
 	public ResponseEntity<OrderedUserInfoResponseV1> getOrderedUserInfo(@PathVariable Long userId);
 }
