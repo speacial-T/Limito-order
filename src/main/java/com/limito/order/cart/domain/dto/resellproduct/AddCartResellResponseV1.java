554af2314f02a6ea8daa042d538a7ea09a652c56
jjaenie <wogus990519@gmail.com>
2025-12-21 20:16:48 +0900

    fix (#60) : 리셀 캐싱은 아이디 정보만 저장하는 방식으로 변경

--- src/main/java/com/limito/order/cart/domain/dto/resellproduct/AddCartResellResponseV1.java
+++ src/main/java/com/limito/order/cart/domain/dto/resellproduct/AddCartResellResponseV1.java
@@ -15,12 +15,5 @@ public class AddCartResellResponseV1 {
 	private UUID optionId;
 	private UUID stockId;
 	private UUID productId;
-	private String productName;
-	private String productColor;
-	private String productSize;
-	private int productPrice;
-	private String brandName;
-	private String thumbnailUrl;
-	private Long sellerId;
 	private ProductType productType;
 }
