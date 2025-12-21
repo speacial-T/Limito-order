f55842ee7731e3c5a72c37946f1b793526269317
jjaenie <wogus990519@gmail.com>
2025-12-21 11:26:48 +0900

    fix (#60) : 리셀 장바구니 추가 요청 DTO, 캐싱 도메인 변경

--- src/main/java/com/limito/order/cart/domain/model/ResellCacheItem.java
+++ src/main/java/com/limito/order/cart/domain/model/ResellCacheItem.java
@@ -17,14 +17,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ResellCacheItem {
 	private UUID optionId;
-	private UUID stockId;
 	private UUID productId;
-	private String productName;
-	private String productColor;
-	private String productSize;
-	private int productPrice;
-	private String brandName;
-	private String thumbnailUrl;
-	private Long sellerId;
 	private ProductType productType;
 }
